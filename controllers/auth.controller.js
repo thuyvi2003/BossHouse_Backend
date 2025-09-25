923f243681e4fe281053312f56d8bfe2ff9eaa97
DESKTOP-55BHRIT\WINDOWS <bonvtce181691@fpt.edu.vn>
2025-09-25 17:46:07 +0700

    BonVT Add backend register + forgot password

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,4 @@
-const { loginService } = require("../services/auth.service.js");
+const { loginService, registerService, resendOTP, verifyOTP, forgotPasswordService, resetPasswordService } = require("../services/auth.services.js");
 
 const login = async (req, res) => {
     try {
@@ -11,4 +11,59 @@ const login = async (req, res) => {
     }
 };
 
-module.exports = { login };
+const register = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const data = await registerService(name, email, password);
+        res.status(201).json(data);
+    } catch (error) {
+        console.error("Error in register controller:", error);
+        res.status(400).json({ message: error.message });
+    }
+};
+
+const verify = async (req, res) => {
+    try {
+        const { email, otp } = req.body;
+        const data = await verifyOTP(email, otp);
+        res.status(201).json(data);
+    } catch (error) {
+        console.error("Error in verify OTP controller:", error);
+        res.status(400).json({ message: error.message });
+    }
+};
+
+const resend = async (req, res) => {
+    try {
+        const { email } = req.body;
+        const data = await resendOTP(email);
+        res.status(200).json(data);
+    } catch (error) {
+        console.error("Error in resend OTP controller:", error);
+        res.status(400).json({ message: error.message });
+    }
+};
+
+const forgotPassword = async (req, res) => {
+    try {
+        const { email } = req.body;
+        const data = await forgotPasswordService(email);
+        res.status(200).json(data);
+    } catch (error) {
+        console.error("Error in forgot password controller:", error);
+        res.status(400).json({ message: error.message });
+    }
+};
+
+const resetPassword = async (req, res) => {
+    try {
+        const { email, token, newPassword, confirmPassword } = req.body;
+        const data = await resetPasswordService(email, token, newPassword, confirmPassword);
+        res.status(200).json(data);
+    } catch (error) {
+        console.error("Error in reset password controller:", error);
+        res.status(400).json({ message: error.message });
+    }
+};
+
+module.exports = { login, register, verify, resend, forgotPassword, resetPassword };
