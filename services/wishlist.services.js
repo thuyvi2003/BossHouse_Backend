adb5acb444375f45e827604e13611cba4d5ba61d
Thuy Vi <vivltce170398@fpt.edu.vn>
2025-10-15 20:19:33 +0700

    ViVLT: Fix function view and add to wishlist

--- services/wishlist.services.js
+++ services/wishlist.services.js
@@ -3,33 +3,40 @@ const Wishlist = require("../models/wishlist.model");
 
 exports.addToWishlist = async (userId, productVariationId, note) => {
   try {
-      const item = await Wishlist.create({
-        user_id: userId,
-        product_variation_id: productVariationId,
-        note,
+    const item = await Wishlist.create({
+      user_id: userId,
+      product_variation_id: productVariationId,
+      note,
     });
-    if(!item) throw new Error('Failed to add to wishlist');
+    if (!item) throw new Error('Failed to add to wishlist');
     console.log("hellloooooooo", item)
     const populatedItem = await Wishlist.findById(item._id)
-        .populate({
-          path: "product_variation_id",
-        })
-         return { success: true, message: "Added to wishlist", data: populatedItem };
+      .populate({
+        path: "product_variation_id",
+        populate: { path: "product_id" }
+      })
+    return { success: true, message: "Added to wishlist", data: populatedItem };
   } catch (error) {
-     if (error.code === 11000) {
-        return {
-          success: false,
-          message: "Product already in wishlist"
-        };
-      }
-      throw error;
+    if (error.code === 11000) {
+      return {
+        success: false,
+        message: "Product already in wishlist"
+      };
     }
+    throw error;
   }
-exports.getWishlist = async (userId) => {
-    const wishList = await Wishlist.find({user_id: userId})
-      .populate("product_variation_id")
-      .sort({ created_at: -1 }); //Loc san pham moi them vao wishlist len dau
-      if(!wishList) throw new Error('Failed to get wishlist');
-      return wishList;
+}
+exports.getWishlist = async (userId, page, limit) => {
+  const skip = (page - 1) * limit;
+  const wishlist = await Wishlist.find({ user_id: userId })
+    .populate({
+      path: "product_variation_id",
+      populate: { path: "product_id" },
+    })
+    .skip(skip)
+    .limit(limit)
+    .sort({ created_at: -1 });
+  const total = await Wishlist.countDocuments({ user_id: userId });
+  return { wishlist, total };
 }
 
